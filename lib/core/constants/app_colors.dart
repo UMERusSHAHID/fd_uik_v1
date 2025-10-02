bda3c86381f446787c1809984e1c52c7d201be21
Muzammil <muzammilrashid3108@gmail.com>
2025-10-02 21:12:17 +0500

    home init

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Color(0xffFA4A0C);
   static const primaryOnWhite = Color(0xffFF460A);
-  static const backGround = Color(0xffEDEDED);
+  static const backGround1 = Color(0xffEDEDED);
+  static const backGround = Color(0xffF2F2F2);
   static const welcomeBackground = Color(0xffFF4B3A);
   static const white = Color(0xffEDEDED);
+  static const blackText = Color(0xff000000);
+  static const textFieldBackground = Color(0xffEFEEEE);
+  static const searchHint = Color(0xff787777);
 }
--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_delivery_kit/core/constants/app_colors.dart';
+import 'package:food_delivery_kit/core/constants/app_image_strings.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +10,69 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, top: 74).r,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(AppImageStrings.menu),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 50.0).r,
+                    child: SvgPicture.asset(AppImageStrings.shoppingCart),
+                  ),
+                ],
+              ),
+              SizedBox(height: 46.h),
+              Row(
+                children: [
+                  Text(
+                    "Delicious\nfood for you",
+                    style: TextStyle(
+                      color: AppColors.blackText,
+                      fontSize: 34.sp,
+                      fontFamily: 'SfProRounded',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 28,),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  height: 60.h,
+                  width: 314.w,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColors.textFieldBackground,
+                      prefixIcon: SizedBox(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 35,top: 18,bottom: 18,right: 16).r,
+                            child: SvgPicture.asset(AppImageStrings.search),
+                          )),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.r),
+                        borderSide: BorderSide.none
+                      ),
+                      labelText: "Search",
+                      labelStyle: TextStyle(
+                        color: AppColors.searchHint,
+                        fontSize: 17.sp,
+                        fontWeight: FontWeight.w600
+                      )
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
